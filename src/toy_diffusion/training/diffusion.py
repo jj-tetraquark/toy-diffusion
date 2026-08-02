983a1da29f2ed1d56bb29043242d931c8f58bdf9
Juno Dark <jj@tetraquark.xyz>
2026-08-02 01:08:21 +0100

    first pass diffusion lightning module

---
+++ src/toy_diffusion/training/diffusion.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn.functional as F
+import lightning as L
+
+def linear_beta_schedule(timestamps, start=0.0001, end=0.02):
+    return torch.linspace(start, end, timestamps)
+
+def cosine_beta_schedule(timestamps, s=0.008):
+    t = torch.arange(timestamps + 1)
+    f_x = torch.cos((t/timestamps + s)/(1 + s) * torch.pi/2)**2
+    a_t = f_x/f_x[0]
+    B_t = 1 - a_t[1:]/(a_t[:-1])
+    return torch.clip(B_t, 1e-4, 0.9999)
+
+
+class DiffusionModule(L.LightningModule):
+    def __init__(self, model, beta_schedule, timesteps, lr, noise=None):
+        super().__init__()
+        self.model = model
+        self.timesteps = timesteps
+
+        self._lr = lr
+
+        betas = beta_schedule(timesteps)
+        self._alphas_cumprod = (1 - betas).cumprod(dim=0)
+
+        if noise is None:
+            self._noise = torch.randn_like
+
+
+    def add_noise(self, x_0, t):
+        noise = self._noise(x_0)
+
+        a_bar = self._alphas.cumprod[t].view(-1, 1, 1, 1)
+        x_t = a_bar.sqrt() * x_0 + (1 - a_bar).sqrt() * noise
+
+        return x_t
+
+    def _do_step(self, batch, batch_idx):
+        x = batch
+        batch_size = x.shape[0]
+
+        t = torch.randint(0, self.timesteps, (batch_size,1)).long()
+
+        noised_images = self.add_noise(x, t)
+        noise_pred(noisy_images, t)
+
+        loss = F.mse_loss(noise_pred, noise)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self._do_step(batch, batch_idx)
+
+    def validation_step(self, batch, batch_idx):
+        return self._do_step(batch, batch_idx)
+
+    def configure_optimizers(self):
+        return {
+            "optimizer": torch.optim.Adam(self.model.parameters, lr=self._lr),
+            "lr_scheduler": torch.optim.lr_scheduler.CosineAnnealingLR
+        }
